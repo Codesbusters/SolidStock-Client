dff2a5da014f8d227f435fa68e06f55c4f6a44af
Dorian Breuillard <dodolereporter@gmail.com>
2023-10-17 16:27:27 +0200

    Menu avec Icons + logo

--- src/main/java/io/github/palexdev/materialfx/demo/Demo.java
+++ src/main/java/io/github/palexdev/materialfx/demo/Demo.java
@@ -26,9 +26,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import org.scenicview.ScenicView;
 
 public class Demo extends Application {
@@ -42,8 +40,6 @@ public class Demo extends Application {
         Parent root = loader.load();
         Scene scene = new Scene(root);
         MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
-        scene.setFill(Color.TRANSPARENT);
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setScene(scene);
         primaryStage.setTitle("MaterialFX Demo");
         primaryStage.show();
