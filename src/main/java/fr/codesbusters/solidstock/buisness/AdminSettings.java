c700932f26e959fdb7ccf4bce22629e4a4c8d16f
Dorian CARDOSO <doriancardoso00@gmail.com>
2023-11-23 19:46:13 +0100

    admin settings page

---
+++ src/main/java/fr/codesbusters/solidstock/buisness/AdminSettings.java
@@ -0,0 +1,17 @@
+package fr.codesbusters.solidstock.buisness;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class AdminSettings {
+
+    private String urlApi;
+
+    private String urlWeb;
+
+}
