4e78ceac6333f131fda91f198fbef7a8a7766a9d
Dorian CARDOSO <doriancardoso00@gmail.com>
2024-04-05 18:23:39 +0200

    Admin settings handle image get and put

--- src/main/java/fr/codesbusters/solidstock/controller/adminSettings/AdminSettingsController.java
+++ src/main/java/fr/codesbusters/solidstock/controller/adminSettings/AdminSettingsController.java
@@ -10,10 +10,18 @@ import fr.codesbusters.solidstock.service.RequestAPI;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -69,6 +77,11 @@ public class AdminSettingsController extends DefaultController implements Initia
     @FXML
     MFXTextField urlWeb;
 
+    @FXML
+    ImageView ownerCompanyImage;
+
+    private File imageSelected;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         reloadAdminSettings();
@@ -95,7 +108,14 @@ public class AdminSettingsController extends DefaultController implements Initia
         RequestAPI requestAPI = new RequestAPI();
         ResponseEntity<String> ownerCompanyResponse = requestAPI.sendPutRequest("/owner-company", ownerCompany, String.class, true);
         if (ownerCompanyResponse.getStatusCode().is2xxSuccessful()) {
-            openDialog(stackPane.getScene(), "Paramètres administrateurs sauvegardés avec succès", DialogType.INFORMATION, 0);
+            MultiValueMap<String, Object> requestBody = new LinkedMultiValueMap<>();
+            requestBody.add("file", new FileSystemResource(imageSelected));
+            ResponseEntity<String> ownerCompanyImageResponse = requestAPI.sendPutRequestWithFile("/owner-company/image", requestBody, String.class, true);
+            if (ownerCompanyImageResponse.getStatusCode().is2xxSuccessful()) {
+                openDialog(stackPane.getScene(), "Paramètres administrateurs sauvegardés avec succès", DialogType.INFORMATION, 0);
+            } else {
+                openDialog(stackPane.getScene(), "Une erreur est survenue lors de la sauvegarde de l'image", DialogType.ERROR, 0);
+            }
         } else {
             openDialog(stackPane.getScene(), "Une erreur est survenue lors de la sauvegarde de vos paramètres", DialogType.ERROR, 0);
         }
@@ -129,5 +149,34 @@ public class AdminSettingsController extends DefaultController implements Initia
         ownerCompanyEmail.setText(adminSettings.getEmail());
         ownerCompanyPhone.setText(adminSettings.getPhone());
         ownerCompanyIban.setText(adminSettings.getIban());
+
+        File ownerCompanyImageFile = null;
+        try {
+            ownerCompanyImageFile = requestAPI.getImage("/owner-company/image", true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (ownerCompanyImageFile != null) {
+            log.info("Image found");
+            Image image = new Image(ownerCompanyImageFile.toURI().toString());
+            ownerCompanyImage.setImage(image);
+        }
+    }
+
+    @FXML
+    public void selectOwnerCompanyImage() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Images", "*.png", "*.jpg", "*.jpeg", "*.gif")
+        );
+
+        // Afficher la boîte de dialogue et obtenir le fichier sélectionné
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            Image image = new Image(selectedFile.toURI().toString());
+            ownerCompanyImage.setImage(image);
+            imageSelected = selectedFile;
+        }
     }
 }
